52f276e21c635047fad378fbbc019c7a6a7e8136
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2023-02-04 14:53:54 -0400

    task buttons styles

--- src/Task.tsx
+++ src/Task.tsx
@@ -1,11 +1,11 @@
 import styles from "./Task.module.css";
-import { Circle, Trash, CheckCircle } from "phosphor-react";
+import { Trash, Check } from "phosphor-react";
 
 export function Task() {
   return (
     <li className={styles.container}>
-      <button className={styles.checkButton}>
-        <Circle size={24} />
+      <button className={styles.checkButtonActive}>
+        <Check size={16} weight="bold" />
       </button>
       <p>Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
       <button className={styles.trashButton}>
