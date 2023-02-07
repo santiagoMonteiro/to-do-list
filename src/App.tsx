2ef8bc663c4573e2825af32d055975979593ea89
Santiago Monteiro <santiago.a.monteiro@gmail.com>
2023-02-06 22:33:20 -0400

    add task js logic

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,55 @@ import { Header } from "./Header";
 import { TaskList } from "./TaskList";
 import { GlobalTaskInfo } from "./GlobalTaskInfo";
 import { NoTaskNotification } from "./NoTaskNotification";
+import { ChangeEvent, FormEvent, useState } from "react";
+
+interface Task {
+  description: string;
+  completed: boolean;
+}
 
 export function App() {
+  const [newTaskText, setNewTaskText] = useState("");
+  const [taskList, setTaskList] = useState<Task[]>([]);
+
+  console.log(taskList);
+
+  const isNewTaskEmpty = newTaskText.length === 0;
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    setNewTaskText(event.target.value);
+  }
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault();
+
+    const newTask = {
+      description: newTaskText,
+      completed: false,
+    };
+
+    setTaskList([...taskList, newTask]);
+    setNewTaskText("");
+  }
+
   return (
     <>
       <Header />
       <main className={styles.wrapper}>
-        <form className={styles.taskForm}>
+        <form className={styles.taskForm} onSubmit={handleCreateNewTask}>
           <input
             className={styles.taskInput}
             type="text"
             placeholder="Adicione uma nova tarefa"
+            value={newTaskText}
+            onChange={handleNewTaskChange}
+            autoFocus
           />
-          <button className={styles.addTaskButton} type="submit">
+          <button
+            className={styles.addTaskButton}
+            type="submit"
+            disabled={isNewTaskEmpty}
+          >
             <span>Criar</span>
             <PlusCircle size={16} weight="bold" />
           </button>
@@ -25,9 +61,7 @@ export function App() {
 
         <GlobalTaskInfo />
 
-        {/* <NoTaskNotification /> */}
-
-        <TaskList />
+        {taskList.length === 0 ? <NoTaskNotification /> : <TaskList />}
       </main>
     </>
   );
